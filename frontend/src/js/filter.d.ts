faced78818f3eb56a63b5e0f61fb0678a159970a
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-08-26 10:22:08 +0900

    jsファイルを削除。

--- frontend/src/js/filter.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Filter {
-    regex: RegExp;
-    constructor(list: string[]);
-    setFilterList(list: string[]): RegExp;
-    filtering(sentence: string): string;
-    isPure(sentence: string): boolean;
-}
--- frontend/src/js/searchEngine.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class SearchEngine {
-    ref: string;
-    field: string;
-    docs: any[];
-    constructor(ref: string, field: string);
-    add(doc: any): void;
-    search(text: string): any[];
-}
